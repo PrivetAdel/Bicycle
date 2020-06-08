6140fff67d61412ae4a0a7881ab8301d247156a6
Adel <rizvanovaadel@gmail.com>
2020-06-08 14:23:38 +0300

    Updates

--- js/scroll.js
+++ js/scroll.js
@@ -3,11 +3,6 @@
 })();
 
 function scrollTo() {
-	const links = document.getElementsByTagName('a');
-	for (let i = 0; i < links.length; i++) {
-		const link = links[i];
-		if ((link.href && link.href.indexOf('#') != -1) && ((link.pathname == location.pathname) || ('/' + link.pathname == location.pathname)) && (link.search == location.search)) {
-			link.onclick = scrollAnchors;
-		}
-	}
+	const links = document.querySelectorAll(`.scroll`);
+	links.forEach(link => link.onclick = scrollAnchors;)
 }
